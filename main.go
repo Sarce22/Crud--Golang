961b4955b275c352fd7cf393de88acb8c6a4f760
Sarce22 <sebastianarce482@gmail.com>
2025-05-05 10:46:25 -0500

    changemain

--- main.go
+++ main.go
@@ -8,10 +8,11 @@ import (
 func main() {
 	// Configuración de la ruta
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World! 🚀"))
+		w.Write([]byte("Hello, World and Docker! 🚀"))
 	})
 
 	log.Println("🚀 Servidor corriendo en http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
+
 // En este código, hemos creado un servidor HTTP simple que responde con "Hello, World! 🚀" en la ruta raíz.
